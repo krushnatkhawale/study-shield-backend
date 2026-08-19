dd8d340a5d7de356a0183524a92fdc7193efb8d0
Krushnat <krushnatkhawale@gmail.com>
2026-08-19 22:56:24 +0530

    refactoring overall

--- ss-modulith/src/main/java/com/studyshield/studyshield/StudyShieldApplication.java
+++ ss-modulith/src/main/java/com/studyshield/studyshield/StudyShieldApplication.java
@@ -1,12 +1,19 @@
 package com.studyshield.studyshield;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudyShieldApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(StudyShieldApplication.class);
+
     public static void main(String[] args) {
+        String dbUrl = System.getenv("DATABASE_URL");
+        String dbUser = System.getenv("DB_USERNAME");
+        log.info("DB_URL set: {}, DB_USERNAME set: {}", dbUrl != null, dbUser != null);
         SpringApplication.run(StudyShieldApplication.class, args);
     }
 }
